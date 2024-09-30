a0a70e4c986e36d207c190206c92691f4f93b4ea
William0305 <williamnascimentoacre2015@gmail.com>
2024-09-30 15:51:57 -0500

    feat

---
+++ Atividade 01/src/Atividade01.java
@@ -0,0 +1,47 @@
+public class Atividade01 {
+    public static void main(String[] args) throws Exception {
+        int inteiro1 = 1;
+        int inteiro2 = 2;
+        int inteiro3 = 3;
+        int a = 4;
+        int b = 7;
+        int inteiro4 = 6;
+
+        float flutuante2 = 1;
+        float flutuante1 = 2;
+        float flutuante3 = 5;
+        double flutua1 = 9;
+        double flutua2 = 3;
+        double flutua3 = 1;
+
+
+        //1. Realize e exiba a soma de dois números inteiros (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 1: " + (inteiro1 + inteiro2));
+
+        // 2. Realize e exiba a multiplicação de dois números inteiros (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 2: " + (inteiro1 * inteiro3));
+
+        // 3. Realize e exiba a divisão de dois números inteiros (Obs: pode utilizar dois números fixos)
+        
+        System.out.println("Resposta da 3: " + (inteiro4 / inteiro2));
+ 
+        // 4. Realize e exiba a multiplicação de dois números inteiros (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 4: " + (inteiro1 * inteiro3));
+
+        // 5. Realize e exiba o resto da divisão de dois números inteiros (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 5: " + (b  % a));
+
+        // 6. Realize e exiba a soma de dois números ponto flutuante do tipo FLOAT (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 6: " + (flutuante1 + flutuante2));
+
+        // 7. Realize e exiba a subtração de dois números ponto flutuante do tipo FLOAT (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 7: " + (flutuante3 - flutuante1));
+
+        // 8. Realize e exiba a divisão de dois números ponto flutuante do tipo DOUBLE (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 8: " + (flutua1 / flutua2));
+
+        // 9. Realize e exiba a multiplicação de dois números ponto flutuante do tipo DOUBLE (Obs: pode utilizar dois números fixos)
+        System.out.println("Resposta da 9: " + (flutua2 * flutua3));
+    }
+    
+}
---
+++ Atividade 01/src/Variaveis.java
@@ -0,0 +1,21 @@
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+        //Variáveis e tipos primitivos
+
+        //Inteiros
+
+        Integer numeroInteiro;
+        int numeroInteiro2;
+
+        //Ponto flutuante 
+
+        Double numeroFlutuanteGrande;
+        float numeroFlutuando;
+
+        //Booleano 
+
+        Boolean verdadeiro = true;
+        boolean falso = false;
+
+    }
+}
